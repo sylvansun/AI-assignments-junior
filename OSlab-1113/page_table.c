e00f97f99440693478f6012b58b6865d5f091056
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-11-08 19:11:25 +0800

    Update page_table.c

--- OSlab-1113/page_table.c
+++ OSlab-1113/page_table.c
@@ -293,17 +293,17 @@ int unmap_range_in_pgtbl(void *pgtbl, vaddr_t va, size_t len)
          * mark the final level pte as invalid. Iterate until all pages are
          * unmapped.
          */
-    ptp_t* ptp = NULL;
-    pte_t* pte = NULL;
+    ptp_t* cur_ptp = (ptp_t *) pgtbl;
+    pte_t* pte;
     int result;
 
     for (const vaddr_t end_va = va + len; va < end_va; va += PAGE_SIZE) {
         cur_ptp = (ptp_t *) pgtbl;
         for(int level=0; level<3;++level){
-            result = get_next_ptp(ptp, level, va, &ptp, &pte, true);
+            result = get_next_ptp(cur_ptp, level, va, &cur_ptp, &pte, true);
             if(result == -ENOMAPPING){return result;}
         }
-        ptp->ent[GET_L3_INDEX(va)]->l3_page.is_valid = 0;
+        (cur_ptp->ent[GET_L3_INDEX(va)])->l3_page.is_valid = 0;
     };
 
     return 0;
