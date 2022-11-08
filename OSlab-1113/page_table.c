86440c9adcff30e8052a01252a2497c9e04f15d9
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-11-08 20:36:58 +0800

    Update page_table.c

--- OSlab-1113/page_table.c
+++ OSlab-1113/page_table.c
@@ -315,8 +315,8 @@ int map_range_in_pgtbl_huge(void *pgtbl, vaddr_t va, paddr_t pa, size_t len,
                             vmr_prop_t flags)
 {
         /* LAB 2 TODO 4 BEGIN */
-    ptp_t* ptp_0 = (ptp_t *) pgtbl, *ptp_1, *ptp_2, *ptp_3;
-    pte_t* pte_0, *pte_1, *pte_2, *pte_3;
+    ptp_t* ptp;
+    pte_t* pte;
     int ret = 0;
     const vaddr_t end_va = va + len;
 
@@ -324,15 +324,16 @@ int map_range_in_pgtbl_huge(void *pgtbl, vaddr_t va, paddr_t pa, size_t len,
     u64 page_shift_1G = PAGE_SHIFT + PAGE_ORDER + PAGE_ORDER;
 
     while (va + page_size_1G < end_va) {
-            ret = get_next_ptp(ptp_0, 0, va, &ptp_1, &pte_0, true);
-            if (ret < 0)
-                    return ret;
+        ptp = (ptp_t *)pgtbl;
+        ret = get_next_ptp(ptp, 0, va, &ptp, &pte, true);
+        if (ret < 0)
+                return ret;
 
-            pte_1 = &(ptp_1->ent[GET_L1_INDEX(va)]);
-            pte_1->l1_block.is_valid = 1;
-            pte_1->l1_block.is_table = 0;
-            pte_1->l1_block.pfn = pa >> page_shift_1G;
-            set_pte_flags(pte_1, flags, USER_PTE);
+            pte = &(ptp_1->ent[GET_L1_INDEX(va)]);
+            pte->l1_block.is_valid = 1;
+            pte->l1_block.is_table = 0;
+            pte->l1_block.pfn = pa >> page_shift_1G;
+            set_pte_flags(pte, flags, USER_PTE);
 
             va += page_size_1G;
             pa += page_size_1G;
@@ -343,18 +344,19 @@ int map_range_in_pgtbl_huge(void *pgtbl, vaddr_t va, paddr_t pa, size_t len,
     u64 page_shift_2M = PAGE_SHIFT + PAGE_ORDER;
 
     while (va + page_size_2M < end_va) {
-            ret = get_next_ptp(ptp_0, 0, va, &ptp_1, &pte_0, true);
+        ptp = (ptp_t *)pgtbl;
+            ret = get_next_ptp(ptp, 0, va, &ptp, &pte, true);
             if (ret < 0)
                     return ret;
-            ret = get_next_ptp(ptp_1, 1, va, &ptp_2, &pte_1, true);
+            ret = get_next_ptp(ptp, 1, va, &ptp, &pte, true);
             if (ret < 0)
                     return ret;
             
-            pte_2 = &(ptp_2->ent[GET_L2_INDEX(va)]);
-            pte_2->l2_block.is_valid = 1;
-            pte_2->l2_block.is_table = 0;
-            pte_2->l2_block.pfn = pa >> page_shift_2M;
-            set_pte_flags(pte_2, flags, USER_PTE);
+            pte = &(ptp_2->ent[GET_L2_INDEX(va)]);
+            pte->l2_block.is_valid = 1;
+            pte->l2_block.is_table = 0;
+            pte->l2_block.pfn = pa >> page_shift_2M;
+            set_pte_flags(pte, flags, USER_PTE);
 
             va += page_size_2M;
             pa += page_size_2M;
