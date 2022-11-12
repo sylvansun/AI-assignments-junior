1a3090d382816d384687a06927fa61bd00c4654c
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-11-12 11:20:35 +0800

    task2 initial model and dataset

--- DLhw2/dataset.py
+++ DLhw2/dataset.py
@@ -1,9 +1,26 @@
 from jittor.dataset.cifar import CIFAR10
+from model import Classifier
+import jittor as jt
 
-a = CIFAR10()
-a.set_attrs(batch_size=1)
-print(len(a.data))
-print(len(a.targets))
-for imgs, labels in a:
-    print(imgs.shape, labels)
-    break
+
+class mCIFAR10(CIFAR10):
+    def __init__(self, 
+                 train=True):
+        super(mCIFAR10, self).__init__(train=train, download=False)
+        self.set_attrs(batch_size=4)
+
+
+
+if __name__ == "__main__":
+    
+    train_data = mCIFAR10(train=True)
+    test_data = mCIFAR10(train=False)
+    model = Classifier()
+
+    
+    input = jt.randn(4, 3, 32, 32)
+    print(len(train_data.data), len(test_data.data))
+    for imgs, labels in train_data:
+        print(imgs.shape, labels)
+        break
+    
---
+++ DLhw2/model.py
@@ -0,0 +1,41 @@
+from jittor import Module, nn
+
+
+class Classifier(Module):
+    def __init__(self, num_classes=10):
+        super(Classifier, self).__init__()
+        self.conv1 = nn.Conv(in_channels=3, out_channels=64, kernel_size=5)
+        self.bn1 = nn.BatchNorm(num_features=64)
+        
+        self.conv2 = nn.Conv(in_channels=64, out_channels=256, kernel_size=5)
+        self.bn2 = nn.BatchNorm(num_features=256)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.fc1 = nn.Linear(256 * 5 * 5, 256)
+        self.fc2 = nn.Linear(256, 96)
+        self.fc3 = nn.Linear(96, num_classes)
+
+    def execute(self, x):
+        print(x.shape)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = nn.relu(x)
+        x = self.pool(x)
+        
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = nn.relu(x)
+        x = self.pool(x)
+        
+        x = nn.flatten(x, 1)
+        x = self.fc1(x)
+        x = nn.relu(x)
+        x = self.fc2(x)
+        x = nn.relu(x)
+        y = self.fc3(x)
+        return y
+
+
+if __name__ == "__main__":
+    model = Classifier()
+    print(model)
