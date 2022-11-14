e5418252781cdabe5808d8dedf3133c0e89b1d05
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-11-14 22:00:50 +0800

    task3 initialization

---
+++ DLhw3/dataset.py
@@ -0,0 +1,51 @@
+import os
+import pickle
+import numpy as np
+from jittor.dataset import Dataset
+import jittor as jt
+
+
+class CIFAR10(Dataset):
+    base_folder = "cifar-10-batches-py"
+    train_list = ["data_batch_1", "data_batch_2", "data_batch_3", "data_batch_4", "data_batch_5"]
+    test_list = ["test_batch"]
+    meta = {"filename": "batches.meta", "key": "label_names"}
+
+    def __init__(self, train=True, batch_size=64, drop_last=False, shuffle=True, root="cifar_data/"):
+        super(CIFAR10, self).__init__(batch_size=batch_size, drop_last=drop_last, shuffle=shuffle)
+        self.root = root
+        self.train = train
+        if self.train:
+            downloaded_list = self.train_list
+        else:
+            downloaded_list = self.test_list
+        self.data, self.targets = [], []
+        for file_name in downloaded_list:
+            file_path = os.path.join(self.root, self.base_folder, file_name)
+            with open(file_path, "rb") as f:
+                entry = pickle.load(f, encoding="latin1")
+                self.data.append(entry["data"])
+                if "labels" in entry:
+                    self.targets.extend(entry["labels"])
+                else:
+                    self.targets.extend(entry["fine_labels"])
+        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
+
+    def _load_meta(self):
+        path = os.path.join(self.root, self.base_folder, self.meta["filename"])
+        with open(path, "rb") as infile:
+            data = pickle.load(infile, encoding="latin1")
+            self.classes = data[self.meta["key"]]
+        self.class_to_idx = {_class: i for i, _class in enumerate(self.classes)}
+
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
+        return img, target
+
+    def __len__(self):
+        return len(self.data)
+
+
+if __name__ == "__main__":
+
+    train_data = CIFAR10(train=True, batch_size=64)
---
+++ DLhw3/utils/parser.py
@@ -0,0 +1,32 @@
+import argparse
+
+
+def make_parser():
+    parser = argparse.ArgumentParser(description="Task2 Configurations")
+
+    parser.add_argument("--bs", type=int, default=64, help="batch size")
+    parser.add_argument("--lr", type=float, default=1e-4, help="learning rate")
+    parser.add_argument("--wd", type=float, default=1e-4, help="weight decay")
+    parser.add_argument("--ne", type=int, default=200, help="number of epochs")
+    parser.add_argument("--debug", action="store_true", help="run in debug mode")
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="default",
+        choices=["default"],
+        help="choose dataset",
+    )
+    parser.add_argument(
+        "--loss",
+        type=str,
+        default="cross_entropy",
+        choices=["cross_entropy"],
+        help="loss function",
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    parser = make_parser()
+    args = parser.parse_args()
+    print(args)
