16be4fbef06460d0623d6c85c14e11bd7982bcf7
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-11-15 13:57:32 +0800

    support permutation

--- DLhw3/dataset.py
+++ DLhw3/dataset.py
@@ -5,14 +5,14 @@ from jittor.dataset import Dataset
 import jittor as jt
 
 
-class CIFAR10(Dataset):
+class CIFAR10P(Dataset):
     base_folder = "cifar-10-batches-py"
     train_list = ["data_batch_1", "data_batch_2", "data_batch_3", "data_batch_4", "data_batch_5"]
     test_list = ["test_batch"]
     meta = {"filename": "batches.meta", "key": "label_names"}
 
     def __init__(self, train=True, batch_size=64, drop_last=False, shuffle=True, root="cifar_data/"):
-        super(CIFAR10, self).__init__(batch_size=batch_size, drop_last=drop_last, shuffle=shuffle)
+        super(CIFAR10P, self).__init__(batch_size=batch_size, drop_last=drop_last, shuffle=shuffle)
         self.root = root
         self.train = train
         if self.train:
@@ -30,6 +30,9 @@ class CIFAR10(Dataset):
                 else:
                     self.targets.extend(entry["fine_labels"])
         self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
+        self._load_meta()
+        self.H = self.data.shape[2] // 2
+        self.W = self.data.shape[3] // 2
 
     def _load_meta(self):
         path = os.path.join(self.root, self.base_folder, self.meta["filename"])
@@ -39,13 +42,27 @@ class CIFAR10(Dataset):
         self.class_to_idx = {_class: i for i, _class in enumerate(self.classes)}
 
     def __getitem__(self, index):
-        img, target = self.data[index], self.targets[index]
-        return img, target
+        img = self.data[index]
+        cutted = [img[:, :self.H, :self.W], img[:, :self.H, self.W:], img[:, self.H:, :self.W], img[:, self.H:, self.W:]]
+        stacked = np.stack(cutted, axis=0)
+        permutation_order = jt.randperm(4)
+        permuted = stacked[permutation_order]
+        label = np.zeros(shape=(4, 4), dtype=np.int32)
+        for i in range(4):
+            label[i, permutation_order[i]] = 1
+        return permuted, label.reshape(-1, 1)
 
     def __len__(self):
         return len(self.data)
+          
 
 
 if __name__ == "__main__":
 
-    train_data = CIFAR10(train=True, batch_size=64)
+    train_data = CIFAR10P(train=True, batch_size=64)
+    img, label = train_data[0]
+    
+    for _, (img, label) in enumerate(train_data):
+        print(img.shape)
+        print(label.shape)
+        break
